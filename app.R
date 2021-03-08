34833d6a6783d780bfebb774f3275b1eb25f376f
Meier <yd95ubyl@fauad.fau.de>
2021-03-08 14:16:23 +0100

    Simple Commit to test connection

--- app.R
+++ app.R
@@ -15,7 +15,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Covid 19 Simulation"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
